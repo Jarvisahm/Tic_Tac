8a26b2c2a78ab88306bf6b89abb93efbec54d268
Ahmad Hazzaa <ahmad@example.com>
2024-07-29 19:08:35 +0300

    this is butifull project and the AI very smart

---
+++ tictactoe.py
@@ -0,0 +1,143 @@
+"""
+Tic Tac Toe Player
+"""
+
+import math
+import copy 
+
+X = "X"
+O = "O"
+EMPTY = None
+
+
+def initial_state():
+    """
+    Returns starting state of the board.
+    """
+    return [[EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY],
+            [EMPTY, EMPTY, EMPTY]]
+
+
+def player(board):
+    """
+    Returns player who has the next turn on a board.
+    """
+    x_count = sum(row.count(X) for row in board)
+    o_count = sum(row.count(O) for row in board)
+    if x_count <= o_count:
+        return X
+    else:
+        return O
+
+
+def actions(board):
+    """
+    Returns set of all possible actions (i, j) available on the board.
+    """
+    action_set = set()
+    for i in range(3):
+        for j in range(3):
+            if board[i][j] == EMPTY:
+                action_set.add((i, j))
+    return action_set
+
+
+def result(board, action):
+    """
+    Returns the board that results from making move (i, j) on the board.
+    """
+    if action not in actions(board):
+        raise Exception("Invalid action")
+    
+    new_board = copy.deepcopy(board)
+    i, j = action
+    new_board[i][j] = player(board)
+    return new_board
+
+
+def winner(board):
+    """
+    Returns the winner of the game, if there is one.
+    """
+    lines = [
+       
+        board[0], board[1], board[2],
+        
+        [board[0][0], board[1][0], board[2][0]],
+        [board[0][1], board[1][1], board[2][1]],
+        [board[0][2], board[1][2], board[2][2]],
+        
+        [board[0][0], board[1][1], board[2][2]],
+        [board[0][2], board[1][1], board[2][0]]
+    ]
+    
+    for line in lines:
+        if line[0] == line[1] == line[2] and line[0] != EMPTY:
+            return line[0]
+    
+    return None
+
+
+def terminal(board):
+    """
+    Returns True if game is over, False otherwise.
+    """
+    return winner(board) is not None or all(cell != EMPTY for row in board for cell in row)
+
+
+def utility(board):
+    """
+    Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
+    """
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
+
+
+def minimax(board):
+    """
+    Returns the optimal action for the current player on the board.
+    """
+    def max_value(board):
+        if terminal(board):
+            return utility(board)
+        v = float('-inf')
+        for action in actions(board):
+            v = max(v, min_value(result(board, action)))
+        return v
+
+    def min_value(board):
+        if terminal(board):
+            return utility(board)
+        v = float('inf')
+        for action in actions(board):
+            v = min(v, max_value(result(board, action)))
+        return v
+
+    if terminal(board):
+        return None
+
+    current_player = player(board)
+    best_action = None
+
+    if current_player == X:
+        value = float('-inf')
+        for action in actions(board):
+            move_value = min_value(result(board, action))
+            if move_value > value:
+                value = move_value
+                best_action = action
+    else:
+        value = float('inf')
+        for action in actions(board):
+            move_value = max_value(result(board, action))
+            if move_value < value:
+                value = move_value
+                best_action = action
+    
+    return best_action
